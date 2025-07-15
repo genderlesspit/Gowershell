579da666f89b588e1e67f9cc5571f92e55a611ed
genderlesspit <59433707+genderlesspit@users.noreply.github.com>
2025-07-15 16:28:35 -0500

    commit 2

--- gowershell/__init__.py
+++ gowershell/__init__.py
@@ -1,9 +1,19 @@
+import tempfile
 from pathlib import Path
+import requests
 from loguru import logger as log
-exe: str = Path("./gowershell.exe").exists()
 
-
-
-log.debug(f"Found gowershell.exe at {exe}")
+exe = Path("./gowershell.exe")
+# _PERMALINK = r"https://github.com/genderlesspit/Gowershell/blob/3f5382d9dad949941f9c6c55abb8f0c000547d5b/gowershell.exe"
+# exe = Path(tempfile.gettempdir()) / "gowershell.exe"
+#
+# if not exe.exists():
+#     pl = _PERMALINK
+#     log.warning(f"gowershell.exe not found! Attempting to get from {pl}")
+#     r = requests.get(pl)
+#     path = Path(tempfile.gettempdir()) / "gowershell.exe"
+#     path.write_bytes(r.content)
+#
+# log.success(f"Found gowershell.exe at {exe}")
 
 from .core import Gowershell, gowershell
